3311290c3b7de1d9370dfdcdea1e3b097d115eb5
Alberto Forni <alberto.forn@gmail.com>
2014-07-24 15:43:09 +1000

    modified version 0.5

--- packages/ember-simple-auth-cookie-store/lib/simple-auth-cookie-store/ember.js
+++
@@ -1,8 +0,0 @@
-var global = (typeof window !== 'undefined') ? window : {},
-    Ember = global.Ember;
-
-import initializer from './initializer';
-
-Ember.onLoad('Ember.Application', function(Application) {
-  Application.initializer(initializer);
-});
